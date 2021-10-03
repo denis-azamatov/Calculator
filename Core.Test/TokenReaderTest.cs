d0661a981d4d2c8165d2ecb905b912a1e1da4f09
wickedkillah <48183905+wickedkillah@users.noreply.github.com>
2021-10-03 14:32:06 +0600

    fixed error in tests

--- Core.Test/TokenReaderTest.cs
+++ Core.Test/TokenReaderTest.cs
@@ -7,28 +7,28 @@ namespace Core.Test
     {
         [Theory]
         [MemberData(nameof(SimpleData))]
-        public void ReadTokenSuccess(string expression, double a, Token token, double b)
+        public void ReadToken(string expression, double a, Token token, double b)
         {
             using var reader = new TokenReader(expression);
 
             var number1 = reader.Number;
             reader.NextToken();
-            var oper = reader.Token;
+            var op = reader.Token;
             reader.NextToken();
             var number2 = reader.Number;
 
             Assert.Equal(a, number1);
-            Assert.Equal(token, oper);
+            Assert.Equal(token, op);
             Assert.Equal(b, number2);
         }
 
         public static IEnumerable<object[]> SimpleData =>
             new[]
             {
-                new object[] { "1.0+2.4", 1, Token.Add, 2.4m },
-                new object[] { "3.5-2.2", 3.5, Token.Subtract, 2.2m },
-                new object[] { "3.5*2.2", 3.5, Token.Multiply, 2.2m },
-                new object[] { "3.5/2.2", 3.5, Token.Divide, 2.2m }
+                new object[] { "1.0+2.4", 1, Token.Add, 2.4 },
+                new object[] { "3.5-2.2", 3.5, Token.Subtract, 2.2 },
+                new object[] { "3.5*2.2", 3.5, Token.Multiply, 2.2 },
+                new object[] { "3.5/2.2", 3.5, Token.Divide, 2.2 }
             };
     }
 }
